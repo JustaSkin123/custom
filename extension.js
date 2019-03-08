d727e701c2228ff4b30c23721c4d84459b7276c6
JustaSkin123 <40543348+JustaSkin123@users.noreply.github.com>
2019-03-08 15:00:24 +0100

    Updated links to try and fix console errors

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: 'JustaBot',
         language: 'english',
-        chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
-        scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+        chatLink: 'https://raw.githack.com/JustaSkin123/custom/master/lang/en.json',
+        scriptLink: 'https://raw.githack.com/basicBot/source/master/basicBot.js',
         roomLock: false, // Requires an extension to re-load the script
         startupCap: 200, // 1-200
         startupVolume: 0, // 0-100
@@ -108,13 +108,13 @@
         songstats: false,
         commandLiteral: '!',
         blacklists: {
-            NSFW: 'https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json',
-            OP: 'https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json',
-            BANNED: 'https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json'
+            NSFW: 'https://raw.githack.com/JustaSkin123/custom/master/blacklists/NSFWlist.json',
+            OP: 'https://raw.githack.com/JustaSkin123/custom/master/blacklists/OPlist.json',
+            BANNED: 'https://raw.githack.com/JustaSkin123/custom/master/blacklists/BANNEDlist.json'
         }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://raw.githack.com/basicBot/source/master/basicBot.js", extend);
 
 }).call(this);
